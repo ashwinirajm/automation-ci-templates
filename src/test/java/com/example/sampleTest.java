e7de531fd690b490c4b0884b3e4b6b049e9910bd
Ashwini Raj <37695456+ashwinirajm@users.noreply.github.com>
2026-01-06 02:37:12 +0530

    Implement environment loading test
    Added a test method to load environment properties.

--- src/test/java/com/example/sampleTest.java
+++ src/test/java/com/example/sampleTest.java
@@ -1,1 +1,18 @@
+package com.example;
+
+import org.testng.annotations.Test;
+import java.util.Properties;
+import java.io.FileInputStream;
+
+public class SampleTest {
+
+    @Test
+    public void testEnvironment() throws Exception {
+        String env = System.getProperty("env", "qa");
+        Properties props = new Properties();
+        props.load(new FileInputStream("src/test/resources/" + env + ".properties"));
+        System.out.println("Running test in environment: " + env);
+        System.out.println("Base URL: " + props.getProperty("base.url"));
+    }
+}
 
